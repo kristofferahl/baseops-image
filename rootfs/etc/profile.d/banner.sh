4b2159230bd48c5babd79425ae9869e60b29cf2d
Kristoffer Ahl <mail@77dynamite.com>
2024-02-27 09:54:46 +0100

    feat(banner): making banner script reusable

--- rootfs/etc/profile.d/banner.sh
+++ rootfs/etc/profile.d/banner.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-BANNER_COMMAND='figurine'
+BANNER_DISPLAY=${BANNER_DISPLAY:-true}
 BANNER_INDENT="${BANNER_INDENT:-  }"
 BANNER_FONT="${BANNER_FONT:-Colossal.flf}"
 
 if [[ ${SHLVL} -le 1 ]]; then
   function _header() {
-    if [ -n "${BANNER}" ]; then
+    if [[ -n "${BANNER}" && ${BANNER_DISPLAY:-false} == true ]]; then
       echo
-      ${BANNER_COMMAND} -f "${BANNER_FONT}" "${BANNER}" | sed "s/^/${BANNER_INDENT}/"
+      figurine -f "${BANNER_FONT:?}" "${BANNER:?}" | sed "s/^/${BANNER_INDENT:-}/"
       echo
+      BANNER_DISPLAY=false
     fi
   }
   _header
